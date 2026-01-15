1b0a050d43fb9e3d8c6a31e38ecd6c40e7733ae7
Ian Darwin <ian@darwinsys.com>
2026-01-14 21:40:12 -0500

    Set up for macOS

--- src/main/java/text3d/FreeLoader.java
+++ src/main/java/text3d/FreeLoader.java
@@ -1,13 +1,18 @@
 package text3d;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.SymbolLookup;
+
 /** Standalone tool to try out dynamic loading on various OSes. */
 public class FreeLoader {
 
 	record OS(String name, String libraryPath, String libraryName) {}
 
 	static OS[] oses = {
-		new OS("OpenBSD", "/usr/X11R6/lib", "libfreetype"),
-		new OS("macOS", "/Library/Java/JavaVirtualMachines/temurin-25.jdk/Contents/Home/lib", "libfreetype.dylib"),
+		new OS("OpenBSD", "/usr/X11R6/lib", "libfreetype.so"),
+		// This should be "macOS" or "Darwin" but Java still uses this name,
+		// but we don't have to set the library path
+		new OS("Mac OS X", null, "libfreetype.dylib"),
 	};
 
 	public static void main(String[] args) {
@@ -16,6 +21,8 @@ public class FreeLoader {
 		for (OS os : oses) {
 			if (os.name().equals(osName)) {
 				System.out.println(os);
+				final SymbolLookup LNK = SymbolLookup.libraryLookup(os.libraryName(), Arena.global());
+				System.out.println("LNK = " + LNK);
 				return;
 			}
 		}
